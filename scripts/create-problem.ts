479785cf1b0993086d666a61c7d9674d8a385651
b. vaughn sulit <bvaughnsulit@gmail.com>
2022-11-02 01:22:42 -0700

    dynamically create problem and test files with metadata

--- scripts/create-problem.ts
+++ scripts/create-problem.ts
@@ -2,17 +2,15 @@ import fetch from 'node-fetch'
 import * as dotenv from 'dotenv'
 import path from 'path'
 import fs from 'fs'
+import { convert } from 'html-to-text'
 
 dotenv.config()
 
 const dir = path.resolve(__dirname, '../src')
 
-
 type Question = {
   codeSnippets: { "lang": string, "code": string }[],
   content: string,
-  exampleTestcases: string,
-  sampleTestCase: string,
   questionId: string,
   status: string,
   title: string,
@@ -71,20 +69,67 @@ const getQuestion = async (slug: string): Promise<Question> => {
 }
 
 
-const createQuestionFile = async (question: Question, dir: string) => {
+const createQuestionFile = (question: Question, dir: string) => {
   // assuming that we're always creating typescript files
   let snippet = question.codeSnippets.find(e => e.lang === "TypeScript")
+  const formatOptions = {
+    selectors: [
+      { selector: 'p', options: { leadingLineBreaks: 1, trailingLineBreaks: 1 } },
+      { selector: 'pre', options: { leadingLineBreaks: 1, trailingLineBreaks: 1 } }
+    ]
+  }
+
+  const fileContents =
+`/**
+${question.questionId}. ${question.title}
+https://leetcode.com/problems/${question.titleSlug}
+
+${convert(question.content, formatOptions)}
+
+**/
+
+${snippet ? snippet.code : ''}
+
+export {}
+` 
+
   fs.writeFileSync(
     path.join(dir, question.titleSlug + '.ts'),
-    snippet ? snippet.code : ''
+    fileContents,
+    { flag: 'wx' }
   )
 }
 
 
-(async () => {
-  const slug = '3sum-closest';
+const createQuestionTestFile = (question: Question, dir: string) => {
+  const fileContents =
+`import {} from './${question.titleSlug}'
 
-  const question = await getQuestion(slug)
-  createQuestionFile(question, dir)
-  // createQuestionTestFile(question, dir)
+test('', () => {
+  
+})
+
+` 
+
+  fs.writeFileSync(
+    path.join(dir, question.titleSlug + '.test.ts'), 
+    fileContents,
+    { flag: 'wx' }
+  )
+}
+
+(async () => {
+  const arg = process.argv[2] // skip system args
+  if (arg !== undefined && arg.length > 0) { 
+    const slug = arg.trim()
+    const question = await getQuestion(slug)
+    createQuestionFile(question, dir)
+    createQuestionTestFile(question, dir)
+    console.log('new files created successfully')
+    return
+  }
+  else {
+    console.log('please provide url slug for problem')
+    return
+  }
 })()
