88a069a3a44d5a69855262899adf4c0ff6ede94d
b. vaughn sulit <bvaughnsulit@gmail.com>
2022-11-03 23:36:47 -0700

    working on getting classes to auto import

---
+++ scripts/classes.ts
@@ -0,0 +1,12 @@
+// global.ListNode = class {
+//   val: number
+//   next: ListNode | null
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.next = (next===undefined ? null : next)
+//   }
+// }
+
+global.Foo = (a: string) => { return a } 
+
+
