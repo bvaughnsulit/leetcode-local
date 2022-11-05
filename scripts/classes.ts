2f851800d86cacc36dd145944188e5f42049ab40
b. vaughn sulit <bvaughnsulit@gmail.com>
2022-11-05 01:39:15 -0700

    created template for creating global classes

--- scripts/classes.ts
+++ scripts/classes.ts
@@ -1,12 +1,21 @@
-// global.ListNode = class {
-//   val: number
-//   next: ListNode | null
-//   constructor(val?: number, next?: ListNode | null) {
-//     this.val = (val===undefined ? 0 : val)
-//     this.next = (next===undefined ? null : next)
-//   }
-// }
+String.prototype.customGlobalMethod = function(someArg: string): number {
+  console.log('you called the custom str method with ' + someArg)
+  return 2
+}
 
-global.Foo = (a: string) => { return a } 
+global.myGlobalFunc = (a: string) => {
+  a = 'you called the global func with ' + a
+  console.log(a)
+  return a
+} 
 
-
+global.MyGlobalClass = class MyGlobalClass {
+  myProp: string
+  constructor(myArg: string){
+    this.myProp = 'created new instance of global class with ' + myArg 
+    console.log(this.myProp)
+  }
+  myMethod(x: number): string {
+    return 'called method myMethod with ' + x
+  }
+}
