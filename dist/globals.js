d4b045c80a0e9cd7799a25b0c3584399f54fbc3c
b. vaughn sulit <bvaughnsulit@gmail.com>
2022-11-07 19:51:07 -0800

    removed dist from git

--- dist/globals.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var ListNode = class ListNode {
-    constructor(val, next) {
-        this.val = (val === undefined ? 0 : val);
-        this.next = (next === undefined ? null : next);
-    }
-};
-global.ListNode = ListNode;
